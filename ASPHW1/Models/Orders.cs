c3b40f9f0d965b4fa6db9d4ce529d93b992b0733
changforest <od7581@gmail.com>
2018-03-29 15:32:27 +0800

    model1

---
+++ ASPHW1/Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ASPHW1.Models
+{
+    public class Orders
+    {
+        [DisplayName("訂單編號")]
+        public int OrderID { get;set;}
+        public int CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+
+        public int OrderDate { get; set; }
+    }
+}
